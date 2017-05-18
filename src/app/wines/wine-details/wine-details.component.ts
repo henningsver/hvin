527436398cb3a354311336043b68813996d0469f
henningsver <daniel@henningsver.se>
2017-05-18 10:41:07 +0200

    Continuing with modal

--- src/app/wines/wine-details/wine-details.component.ts
+++ src/app/wines/wine-details/wine-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-wine-details',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class WineDetailsComponent implements OnInit {
 
   constructor() { }
+
+  @Input() wine;
 
   ngOnInit() {
   }
